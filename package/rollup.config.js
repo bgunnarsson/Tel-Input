63f7cfc960657316687bf08633ec48e4d2fb4490
Brynjólfur Gunnarsson <binni@vettvangur.is>
2024-12-18 16:21:00 +0000

    update

--- package/rollup.config.js
+++ package/rollup.config.js
@@ -5,7 +5,7 @@ import { terser } from 'rollup-plugin-terser'
 import copy from 'rollup-plugin-copy'
 
 export default {
-  input: 'src/TelInput.js',
+  input: 'src/index.mjs', // Your input file
   output: [
     {
       file: 'dist/tel-input.esm.js',
@@ -22,18 +22,7 @@ export default {
       format: 'iife',
       name: 'TelInput',
       sourcemap: true,
-      plugins: [
-        terser({
-          format: {
-            comments: false,
-          },
-          compress: {
-            drop_console: true,
-          },
-          // Exclude flags.mjs from minification
-          exclude: [/flags\.mjs$/],
-        }),
-      ],
+      plugins: [terser()], // Apply Terser only for this output
     },
   ],
   plugins: [
@@ -44,7 +33,8 @@ export default {
     json(),
     copy({
       targets: [
-        { src: 'src/flags.mjs', dest: 'dist' }, // Ensure flags.mjs is copied as-is
+        { src: 'src/flags.mjs', dest: 'dist' }, // Copy flags.mjs without transformation
+        { src: 'src/countries.mjs', dest: 'dist' },
       ],
     }),
   ],
