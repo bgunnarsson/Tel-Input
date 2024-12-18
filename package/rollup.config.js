bb11d011a7d3e4ec59673de73553ad3906623080
Brynjólfur Gunnarsson <binni@vettvangur.is>
2024-12-18 16:14:09 +0000

    update

--- package/rollup.config.js
+++ package/rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import { terser } from 'rollup-plugin-terser'
 import json from '@rollup/plugin-json'
+import replace from '@rollup/plugin-replace'
 import copy from 'rollup-plugin-copy'
 import { defineConfig } from 'rollup'
 
